458b7f22b905ccc26364a9dcab660d591ada3d7a
Steve Biedermann <hardliner66@gmail.com>
2025-09-16 21:38:27 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,15 @@
 use anyhow::bail;
 use clap::Parser;
-use git2::{Cred, CredentialType, PushOptions, RemoteCallbacks, Repository, Signature};
+use git2::{Repository, Signature};
 
 #[derive(Parser)]
 struct Cli {
     tag: String,
-    #[arg(short, long)]
-    remote: bool,
 }
 
 use git2::ObjectType;
 
-pub fn delete_remote_tag(
-    repo: &Repository,
-    remote_name: &str,
-    tag_name: &str,
-) -> Result<(), git2::Error> {
-    let mut remote = repo.find_remote(remote_name)?;
-
-    let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(|_url, username_from_url, allowed| {
-        if allowed.contains(CredentialType::SSH_KEY) {
-            return Cred::ssh_key_from_agent(username_from_url.unwrap_or("git"));
-        }
-        if allowed.contains(CredentialType::DEFAULT) {
-            return Cred::default();
-        }
-        Err(git2::Error::from_str("No suitable credentials available"))
-    });
-    callbacks.push_update_reference(|refname, status| {
-        if let Some(msg) = status {
-            eprintln!("Server rejected update for {}: {}", refname, msg);
-        }
-        Ok(())
-    });
-
-    let mut opts = PushOptions::new();
-    opts.remote_callbacks(callbacks);
-
-    let delete_spec = format!(":refs/tags/{}", tag_name);
-    remote.push(&[delete_spec.as_str()], Some(&mut opts))?;
-    Ok(())
-}
-
-fn re_tag(repo: &Repository, tag: &str, new_target_spec: &str, remote: bool) -> anyhow::Result<()> {
+fn re_tag(repo: &Repository, tag: &str, new_target_spec: &str) -> anyhow::Result<()> {
     let refname = format!("refs/tags/{tag}");
 
     // Find the existing tag ref
@@ -65,9 +31,6 @@ fn re_tag(repo: &Repository, tag: &str, new_target_spec: &str, remote: bool) -> 
     };
 
     let _ = repo.tag_delete(tag);
-    if remote {
-        delete_remote_tag(repo, "origin", tag)?;
-    }
 
     let target = repo.revparse_single(new_target_spec)?;
     repo.tag(tag, &target, &tagger, &message, false)?;
@@ -76,8 +39,11 @@ fn re_tag(repo: &Repository, tag: &str, new_target_spec: &str, remote: bool) -> 
 }
 
 fn main() -> anyhow::Result<()> {
-    let Cli { tag, remote } = Cli::parse();
+    let Cli { tag } = Cli::parse();
     let repo = Repository::discover(".")?;
-    re_tag(&repo, &tag, "HEAD", remote)?;
+    re_tag(&repo, &tag, "HEAD")?;
+    eprintln!("Tag successfully recreated");
+    eprintln!("If the previous tag was already pushed, you need to run the following:");
+    println!("git push --delete origin {tag}; git push origin {tag}");
     Ok(())
 }
